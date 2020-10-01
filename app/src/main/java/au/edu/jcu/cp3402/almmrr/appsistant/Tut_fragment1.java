1d3119f758dabbd670e25f511edd3574a310b3c0
Sinminer <robert.sinclair@my.jcu.edu.au>
2020-10-01 12:14:05 +1000

    Started implementing the Tutorial but having issues with creating the intent.

---
+++ app/src/main/java/au/edu/jcu/cp3402/almmrr/appsistant/Tut_fragment1.java
@@ -0,0 +1,29 @@
+package au.edu.jcu.cp3402.almmrr.appsistant;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class Tut_fragment1 extends Fragment {
+
+
+    public Tut_fragment1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tut_fragment1, container, false);
+        // Inflate the layout for this fragment
+
+        return view ;
+    }
+}
--- app/src/main/java/au/edu/jcu/cp3402/almmrr/appsistant/TutorialDialog.java
+++ app/src/main/java/au/edu/jcu/cp3402/almmrr/appsistant/TutorialDialog.java
@@ -2,15 +2,16 @@ package au.edu.jcu.cp3402.almmrr.appsistant;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 
+import android.content.Intent;
 import android.widget.TextView;
 
 public class TutorialDialog {
         Activity activity;
         AlertDialog dialog;
         TextView textView;
-
         TutorialDialog(Activity myActivity) {
             activity = myActivity;
         }
@@ -21,9 +22,9 @@ public class TutorialDialog {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                     if (i == 0) {
-                        //fullTutorial();
+                        fullTutorial();
                     } else if (i == 1) {
-                        //shortTutorial();
+                        shortTutorial();
                     } else skipTutorial();
                 }
 
@@ -32,14 +33,14 @@ public class TutorialDialog {
             dialog.show();
         }
 
-            void setText(String text) {
-            textView.setText(text);
-        }
-
-        void dismiss(){
+        private void skipTutorial(){
             dialog.dismiss();
         }
-        private void skipTutorial(){
+        private void fullTutorial(){
+            Intent intent = new Intent(activity,Tutorial_Activity.class);
+            activity.startActivity(intent);
+        }
+        private void shortTutorial(){
 
         }
     }
