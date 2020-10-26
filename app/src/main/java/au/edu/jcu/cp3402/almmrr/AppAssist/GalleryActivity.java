c3e758a5cd745478c4f1dfc4b358d5e80dd50704
Aidan <aidan.megee@my.jcu.edu.au>
2020-10-26 16:48:24 +1000

    Add GalleryAdapter and GalleryActivity. Add Method for opening Gallery. Add boilerplate methods (commented) for finding an image and creating a new album. Add string resource values for Gallery.

---
+++ app/src/main/java/au/edu/jcu/cp3402/almmrr/AppAssist/GalleryActivity.java
@@ -0,0 +1,70 @@
+package au.edu.jcu.cp3402.almmrr.AppAssist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.content.SharedPreferences;
+
+
+public class GalleryActivity extends AppCompatActivity {
+    private RecyclerView applicationListView;
+    private GalleryAdapter applicationListAdapter;
+    private RecyclerView.LayoutManager applicationListManager;
+    private TutorialDialog tutorialDialog;
+    private String[] applicationList = {
+            "View my Gallery",
+            "Add new Album",
+            "Go to Image",
+    };
+
+    private Class<?>[] applicationActivities = {
+            //Intents for gallery activity
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        final String ACTIVITY_NAME = "Gallery";
+        SharedPreferences appPreferences = getSharedPreferences(getPackageName(), MODE_PRIVATE);
+        boolean colorBlindMode = appPreferences.getBoolean("setting:toggle_color_blind", false);
+        if (colorBlindMode) {
+            setThemeMode(Theme.COLOR_BLIND);
+        } else {
+            setThemeMode(Theme.NORMAL);
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+        applicationListView = findViewById(R.id.view_gallery_list);
+        setApplicationListView();
+
+        int tutorialOption = appPreferences.getInt("setting:option_tutorial_length", -1);
+        tutorialDialog = new TutorialDialog(this, ACTIVITY_NAME);
+        if (tutorialOption == -1) {
+            tutorialDialog.start();
+        } else {
+            tutorialDialog.chooseTutorialOption(tutorialOption);
+        }
+    }
+
+    private void setApplicationListView() {
+        applicationListView.setHasFixedSize(true);
+
+        applicationListManager = new LinearLayoutManager(this);
+        applicationListView.setLayoutManager(applicationListManager);
+
+        applicationListAdapter = new GalleryAdapter(this, applicationList, applicationActivities, applicationListView);
+        applicationListView.setAdapter(applicationListAdapter);
+    }
+
+    private void setThemeMode(Theme theme) {
+        switch (theme) {
+            case COLOR_BLIND:
+                setTheme(R.style.ColorBlindMode);
+                break;
+            default:
+            case NORMAL:
+                setTheme(R.style.AppTheme);
+        }
+    }
+}
